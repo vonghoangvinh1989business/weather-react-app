e3618f6f932839a879716cf0c3e94fba4a8dbe07
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-09-30 16:48:28 +0700

    add code to fetch data from weather api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const api = {
+  key: "364e674bcc19eff44285e4b817e44d10",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      // Process
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (response.ok) {
+          setWeatherInfo(
+            `${data.name}, ${data.sys.country}, ${data.weather[0].description}, ${data.main.temp}`
+          );
+
+          // setWeatherInfo(data);
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+    };
+
+    fetchWeatherData();
+  }, [searchCity]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchCity(searchInput);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
+        />
+        <button>Search</button>
+      </form>
+
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {errorMessage ? (
+            <div style={{ color: "red" }}>{errorMessage}</div>
+          ) : (
+            <div>{weatherInfo}</div>
+            // <div>{console.log(weatherInfo)}</div>
+          )}
+        </>
+      )}
+    </>
   );
 }
 
